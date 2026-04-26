cbddeefec60944e8a020a349f9b4528c15aa15e2
Alan Wallace <alan.wallace@oit.edu>
2026-04-26 08:57:48 -0400

    SCRUM-48 - Write round data to table HTML
    Added ability to dynamically add a new row of HTML to the rounds table

---
+++ scripts/roundsMode.js
@@ -0,0 +1,30 @@
+/*************************************************************************
+ * File: roundsMode.js
+ * This file contains functions that support the "Rounds" mode, 
+ * including the "Log Round" modal.
+*************************************************************************/
+
+/*************************************************************************
+* @function writeRoundToTable
+* @desc 
+* Given an HTML row elemnt and the index of the round to write, write
+* the round to the row element by replacing its innerHTML.
+* @param row -- a reference to an HTML table row
+* @param rIndex -- an integer index into userData.rounds indicating the
+*        round to write to the table.
+* @global GlobalUserData: The data for the current user
+*************************************************************************/
+function writeRoundToTable(row, rIndex) {
+row.innerHTML = "<td>" + GlobalUserData.rounds[rIndex].date + "</td><td>" +
+GlobalUserData.rounds[rIndex].course + "</td><td>" + 
+GlobalUserData.rounds[rIndex].SGS + " (" + GlobalUserData.rounds[rIndex].strokes +
+" in " + GlobalUserData.rounds[rIndex].minutes + ":" + 
+GlobalUserData.rounds[rIndex].seconds + 
+")</td>" +
+"<td><button aria-label='View and Edit Round'" + 
+"onclick='editRound(" + GlobalUserData.rounds[rIndex].roundNum + ")'><span class='fas fa-eye'>" +
+"</span>&nbsp;<span class='fas fa-edit'></span></button></td>" +
+"<td><button aria-label='Delete Round'" + 
+"onclick='confirmDelete(" + GlobalUserData.rounds[rIndex].roundNum + ")'>" +
+"<span class='fas fa-trash'></span></button></td>";
+}
